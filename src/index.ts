11dee064add7b63a0f46ca9e1749913248219a66
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-05-29 10:10:04 -0400

    chore: setup linting

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command';
 
 class Gg extends Command {
-  static description = 'describe the command here'
+  static description = 'describe the command here';
 
   static flags = {
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-  }
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
+  };
 
-  static args = [{name: 'search'}]
+  static args = [{ name: 'search' }];
 
   async run() {
-    const {args} = this.parse(Gg)
-    const {search} = args;
-
+    // const { args } = this.parse(Gg);
+    // const { search } = args;
   }
 }
 
-export = Gg
+export = Gg;
